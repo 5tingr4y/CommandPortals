15eea4b730fee3ea7866cfaa610bdac859c11acb
Raymond Kampmann <raymond.kampmann@udo.edu>
2016-01-25 01:49:15 +0100

    added main class

---
+++ src/net/_5tingr4y/commandportals/CommandPortals.java
@@ -0,0 +1,57 @@
+/*
+This file is part of CommandPortals, licenced under the MIT License (MIT)
+
+Copyright (c) 2016 Raymond Kampmann <http://5tingr4y.net/>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package net._5tingr4y.commandportals;
+
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.spongepowered.api.Game;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameInitializationEvent;
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id = "commandPortals", name = "Command Portals", version = "0.1")
+public class CommandPortals {
+
+    @Inject
+    private Logger logger;
+
+    @Inject
+    private Game game;
+
+    public CommandPortals() {
+        instance = this;
+    }
+
+    @Listener
+    public void onInit(GameInitializationEvent event) {
+        logger.debug("Hello Sponge!");
+    }
+
+    //statics
+    private static CommandPortals instance;
+
+    public static CommandPortals get() {
+        return instance;
+    }
+}
